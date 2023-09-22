d3d189f4fcdbeca206182e58c9600a8fc12c22f2
Daniel <dma_brothers@hotmail.com>
2023-09-22 14:53:41 -0600

    all pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
-
+import mdx from '@astrojs/mdx';
 import tailwind from "@astrojs/tailwind";
+import nodejs from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  // adapter: nodejs({
+  //   mode: 'middleware' // or 'standalone'
+  // }),
+  // output: 'hybrid',
+  integrations: [tailwind(), mdx()],
+  base: "/",
 });
