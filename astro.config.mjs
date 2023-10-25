d5aeee23f1e6da5d1453a8adb7bfd055c2ce2fe8
Daniel <dma_brothers@hotmail.com>
2023-10-25 11:41:38 -0600

    adding sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel/serverless'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
       enabled: true
     }
   }),
-  integrations: [tailwind(), mdx()],
+  site: 'https://ecotecplast.com',
+  integrations: [tailwind(), mdx(), sitemap()],
   base: '/'
 })
